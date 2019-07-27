1f7d0c055b7f7c265d16115c0b0cf7420fbe8d00
albert18 <caps18@hotmail.com>
2019-07-27 12:47:45 -0400

    update

--- 2/DataTypesAndVariables/DataTypesAndVariables/Program.cs
+++ 2/DataTypesAndVariables/DataTypesAndVariables/Program.cs
@@ -11,6 +11,18 @@ namespace DataTypesAndVariables
         static void Main(string[] args)
         {
             //Datatypes int, float and double
+            int num1 = 13;
+            int num2 = 2;
+            int sum = num1 + num2;
+
+            double d1 = 3.5;
+            double d2 = 1.337;
+            double sumD = d1 + d2;
+
+            Console.WriteLine("Sum is " + sum);
+            Console.WriteLine("Sum is " + sumD);
+
+            Console.Read();
 
         }
     }
