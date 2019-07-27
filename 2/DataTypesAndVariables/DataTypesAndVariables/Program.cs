1a0c23c06672ceb201f85b577e9c6d3279250dde
albert18 <caps18@hotmail.com>
2019-07-27 13:32:22 -0400

    upda

--- 2/DataTypesAndVariables/DataTypesAndVariables/Program.cs
+++ 2/DataTypesAndVariables/DataTypesAndVariables/Program.cs
@@ -10,19 +10,63 @@ namespace DataTypesAndVariables
     {
         static void Main(string[] args)
         {
+            #region Datatypes int, float and double
             //Datatypes int, float and double
-            int num1 = 13;
-            int num2 = 2;
-            int sum = num1 + num2;
+            //int num1 = 13;
+            //int num2 = 2;
+            //int sum = num1 + num2;
 
-            double d1 = 3.5;
-            double d2 = 1.337;
-            double sumD = d1 + d2;
+            //double d1 = 3.5;
+            //double d2 = 1.337;
+            //double sumD = d1 + d2;
 
-            Console.WriteLine("Sum is " + sum);
-            Console.WriteLine("Sum is " + sumD);
+            //float f1 = 3.5f;
 
-            Console.Read();
+            //Console.WriteLine("Sum is " + sum);
+            //Console.WriteLine("Sum is " + sumD);
+
+            //Console.Read(); 
+            #endregion
+
+            #region Implicit and Explicit Conversion
+            //int num = 1234151;
+            //long bigNum = num;
+
+            //float myFloat = 13.26f;
+            //double myNewDouble = myFloat;
+
+            //double myDouble = 13.37;
+            //int myInt;
+
+            ////Cast double to int
+            //myInt = (int)myDouble;
+
+            //string myString = myDouble.ToString();
+
+            //Console.WriteLine(myInt);
+            //Console.Read(); 
+            #endregion
+
+            #region Parsing string to int
+            //string myString = "15";
+            //string mySecondString = "13";
+
+            //int num1 = Int32.Parse(myString);
+            //int num2 = Int32.Parse(mySecondString);
+            //int numResult = num1 + num2;
+
+            ////string result = myString + mySecondString;
+
+            //Console.WriteLine(numResult);
+            //Console.Read(); 
+            #endregion
+
+            #region Constants
+            //const cannot be change
+            //const int days = 365;
+
+            //days = 366; 
+            #endregion
 
         }
     }
