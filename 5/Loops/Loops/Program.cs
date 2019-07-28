072b2d98a02483cb91fd412cf57c57a926ac4c1f
albert18 <caps18@hotmail.com>
2019-07-27 21:33:49 -0400

    update

--- 5/Loops/Loops/Program.cs
+++ 5/Loops/Loops/Program.cs
@@ -10,6 +10,33 @@ namespace Loops
     {
         static void Main(string[] args)
         {
+            //for, foreach, while loop and do while
+
+
+            #region For Loop
+            //for (int initial = 0; initial < 10; initial++)
+            //{
+            //    Console.WriteLine(initial);
+            //}
+            //Console.Read();
+
+            //for (int initial = 0; initial < 50; initial+=5)
+            //{
+            //    Console.WriteLine(initial);
+            //}
+            //Console.Read();
+            #endregion
+
+            #region Do While
+            //Reading the code first before condition
+            int counter = 0;
+            do
+            {
+                Console.WriteLine(counter);
+                counter++;
+            } while (counter < 5);
+            Console.Read();
+            #endregion
 
         }
     }
