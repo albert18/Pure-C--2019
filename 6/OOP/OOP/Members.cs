a88acb764b4ad2642a4aaff7fdb1b2a0b165e73a
albert18 <caps18@hotmail.com>
2019-07-28 23:36:02 -0400

    update

---
+++ 6/OOP/OOP/Members.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Members
+    {
+        //private field
+        private string  memberName { get; set; }
+        private string JobTitle { get; set; }
+
+        //public
+        public int Age { get; set; }
+
+
+    }
+}
