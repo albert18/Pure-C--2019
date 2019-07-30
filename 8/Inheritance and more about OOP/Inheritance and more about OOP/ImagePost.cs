6fa797ec26413badcf6696c866ab40a807397587
albert18 <caps18@hotmail.com>
2019-07-29 23:42:43 -0400

    update

--- 8/Inheritance and more about OOP/Inheritance and more about OOP/ImagePost.cs
+++ 8/Inheritance and more about OOP/Inheritance and more about OOP/ImagePost.cs
@@ -6,27 +6,27 @@ using System.Threading.Tasks;
 
 namespace Inheritance_and_more_about_OOP
 {
-    class ImagePost
+    class ImagePost : Post
     {
-        //public string ImageURL { get; set; }
+        public string ImageURL { get; set; }
 
-        //public ImagePost() { }
+        public ImagePost() { }
 
-        //public ImagePost(string title, string sendByUsername, string imageURL, bool isPublic)
-        //{
-        //    // The following properties and the GetNexID method are inherited from Post.
-        //    this.ID = GetNextID();
-        //    this.Title = title;
-        //    this.SendByUsername = sendByUsername;
-        //    this.IsPublic = isPublic;
+        public ImagePost(string title, string sendByUsername, string imageURL, bool isPublic)
+        {
+            // The following properties and the GetNexID method are inherited from Post.
+            this.ID = GetNextID();
+            this.Title = title;
+            this.SendByUsername = sendByUsername;
+            this.IsPublic = isPublic;
 
-        //    // Property ImageUrl is a member of ImagePost, but not of Post
-        //    this.ImageURL = imageURL;
-        //}
+            // Property ImageUrl is a member of ImagePost, but not of Post
+            this.ImageURL = imageURL;
+        }
 
-        //public override string ToString()
-        //{
-        //    return String.Format("{0} - {1} - {2} - by {3}", this.ID, this.Title, this.ImageURL, this.SendByUsername);
-        //}
+        public override string ToString()
+        {
+            return String.Format("{0} - {1} - {2} - by {3}", this.ID, this.Title, this.ImageURL, this.SendByUsername);
+        }
     }
 }
--- 8/Inheritance and more about OOP/Inheritance and more about OOP/Program.cs
+++ 8/Inheritance and more about OOP/Inheritance and more about OOP/Program.cs
@@ -13,19 +13,19 @@ namespace Inheritance_and_more_about_OOP
             Post post1 = new Post("Thanks for the birthday wishes", true, "Denis Panjuta");
             Console.WriteLine(post1.ToString());
 
-            //ImagePost imagePost1 = new ImagePost("Check out my new shoes", "Denis Panjuta",
-            //    "https://images.com/shoes", true);
+            ImagePost imagePost1 = new ImagePost("Check out my new shoes", "Denis Panjuta",
+                "https://images.com/shoes", true);
 
-            //VideoPost videoPost1 = new VideoPost("FailVideo", "Denis Panjuta",
-            //    "https://video.com/failvideo", true, 10);
+            VideoPost videoPost1 = new VideoPost("FailVideo", "Denis Panjuta",
+                "https://video.com/failvideo", true, 10);
 
-            //Console.WriteLine(imagePost1.ToString());
-            //Console.WriteLine(videoPost1.ToString());
+            Console.WriteLine(imagePost1.ToString());
+            Console.WriteLine(videoPost1.ToString());
 
-            //videoPost1.Play();
+            videoPost1.Play();
             Console.WriteLine("Press any key to stop the video!");
             Console.ReadKey();
-            //videoPost1.Stop();
+            videoPost1.Stop();
             Console.ReadLine();
         }
     }
