600a5253146e84e8a7aa9b089cf46804e339d53d
albert18 <caps18@hotmail.com>
2019-07-31 16:46:53 -0400

    update

--- 10/ArrayListsVsLists/ArrayListsVsLists/Program.cs
+++ 10/ArrayListsVsLists/ArrayListsVsLists/Program.cs
@@ -11,6 +11,28 @@ namespace ArrayListsVsLists
     {
         static void Main(string[] args)
         {
+            /////////////////////////////////////////////////////////////////USING LIST
+            List<int> list = new List<int> { 35, 39, 42, 88, 42, 99 };
+
+            Console.WriteLine(list.Contains(99));//return boolean -- looks for an item
+
+            Console.WriteLine(list.Exists(i => i > 65)); //return boolean -- take predicate
+
+            Console.WriteLine(list.Find(i => i > 65)); // find return after 
+
+            List<int> seniorAge = list.FindAll(i => i > 65);// find return list - could return multiple - THE ACTUALLY ITEM
+            foreach (int item in seniorAge)
+            {
+                Console.WriteLine(item);
+            }
+
+            int seniorAge1 = list.FindIndex(i => i == 35);// find return list - could return index 
+            Console.WriteLine(seniorAge1);
+            /////////////////////////////////////////////////////////////////USING LIST
+            ///
+
+
+
             // immutable - limited to one type
             int[] scores = new int[] { 99, 96, 87, 76 };
 
@@ -39,9 +61,9 @@ namespace ArrayListsVsLists
             arrayList.Add("two");
             arrayList.Add("3");
             arrayList.Add(new Number { n = 4 });
-            
 
-            foreach(Object o in arrayList)
+
+            foreach (Object o in arrayList)
             {
                 Console.WriteLine(o);
             }
