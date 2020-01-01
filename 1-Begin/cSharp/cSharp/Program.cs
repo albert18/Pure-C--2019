6609eb0a058359cd3681a3744f70b75f77b6ad27
albert18 <caps18@hotmail.com>
2020-01-01 14:17:31 -0500

    update

--- 1-Begin/cSharp/cSharp/Program.cs
+++ 1-Begin/cSharp/cSharp/Program.cs
@@ -16,37 +16,55 @@ namespace cSharp
     //Which one of the following Datatypes is not considered as "primitive"? - STRING
     //Correct. String is a Class and not a primitive Datatype.It's still a Datatype, thou. It's basically containing multiple Characters.
 
-    //Const are immutable values which are know at compile time and do not change for the life of the program.
+
+    //Const are immutable values which are know at compile time and do not change for the life of the program. [Usually Fields]
+
+    //If you want to convert a number into a string. What Kind of conversion is necessary? (Type-Conversion)
 
 
+    ///////////////////////////////////Sample Code
+    //double myDouble = 13.37;
+    //int myInt;
+    //myInt = (int) myDouble;
+    //Console.WriteLine(myInt);
+
+    //Parsing string to int
+    //string myString = "15";
+    //int myInt = Int32.Parse(myString);
+    //Console.WriteLine(myInt);
 
 
-    //Sample Code
-        //double myDouble = 13.37;
-        //int myInt;
-        //myInt = (int) myDouble;
-        //Console.WriteLine(myInt);
-
-        //Parsing string to int
-        //string myString = "15";
-        //int myInt = Int32.Parse(myString);
-        //Console.WriteLine(myInt);
-
+    //Const are fields
+    //const int number = 9;
 
 
     #endregion
 
+    #region Methods And Try/Catch -- Unary-Operator
 
+    //try − A try block identifies a block of code for which particular exceptions is activated.It is followed by one or more catch blocks.
+    //catch − A program catches an exception with an exception handler at the place in a program where you want to handle the problem.The catch keyword indicates the catching of an exception.
+    //finally − The finally block is used to execute a given set of statements, whether an exception is thrown or not thrown.For example, if you open a file, it must be closed whether an exception is raised or not.
+
+    //Unary-Operators do? - Modify the Value of the related Variable
+    //What is the Return-Type when using Equality-Operators or Conditional Operators? BOOLEAN
+
+
+    //Sample Code
+
+    #endregion
+
+    #region Making Decision
+
+    #endregion
 
 
     class Program
     {
 
-
         static void Main(string[] args)
         {
-
-
+                    
 
 
         }
