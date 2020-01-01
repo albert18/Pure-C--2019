06a59ffcb2af5cfb22eabc797d53cb0f6844df90
albert18 <caps18@hotmail.com>
2020-01-01 13:30:14 -0500

    update

---
+++ 1-Begin/cSharp/cSharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharp
+{
+
+    //DataTypes
+    //int,
+    //float
+    //double,
+    
+
+
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
