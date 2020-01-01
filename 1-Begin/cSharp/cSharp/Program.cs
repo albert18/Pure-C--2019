2cae3dfc49577fad583a8e6eee861cc5c6dca3ef
albert18 <caps18@hotmail.com>
2020-01-01 13:51:42 -0500

    update

--- 1-Begin/cSharp/cSharp/Program.cs
+++ 1-Begin/cSharp/cSharp/Program.cs
@@ -6,12 +6,33 @@ using System.Threading.Tasks;
 
 namespace cSharp
 {
+    #region DataType and Questions
 
     //DataTypes
-    //int,
-    //float
-    //double,
-    
+    //int, float, double, string, bool,
+
+    //What is a Datatype? - A datatype specifies what type of Value in variable can contain.
+
+    //Which one of the following Datatypes is not considered as "primitive"? - STRING
+    //Correct. String is a Class and not a primitive Datatype.It's still a Datatype, thou. It's basically containing multiple Characters.
+
+
+
+
+    //Sample Code
+        //double myDouble = 13.37;
+        //int myInt;
+        //myInt = (int) myDouble;
+        //Console.WriteLine(myInt);
+
+    //Parsing string to int
+        //string myString = "15";
+        //int myInt = Int32.Parse(myString);
+        //Console.WriteLine(myInt);
+
+
+    #endregion
+
 
 
 
@@ -21,6 +42,8 @@ namespace cSharp
 
         static void Main(string[] args)
         {
+
+
 
 
         }
